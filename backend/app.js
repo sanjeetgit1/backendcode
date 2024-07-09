681942ca1724ca42175f5b2d6c77d67e45b5e8ea
sanjeetgit1 <sksanjeet616@gmail.com>
2024-07-09 20:19:29 +0530

    app.js changes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import {userRouter} from "./routes/User.js";
 export const app = express();
-import path from "path";
+
  
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({extended :true, limit:"50mb"}));
@@ -13,8 +13,3 @@ app.use(cookieParser());
 app.use("/api/v1",userRouter);
 
 
-app.use(express.static(path.resolve("./frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("./frontend/build/index.html"));
-});
