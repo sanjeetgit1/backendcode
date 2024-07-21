bd0abbefcbcf027dce230e78c422418fe0095f7b
sanjeetgit1 <sksanjeet616@gmail.com>
2024-07-21 09:53:45 +0530

    app

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ import cookieParser from "cookie-parser";
 import path from "path";
 export const app = express();
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cookieParser());
 
 import { userRouter } from "./routes/User.js";
