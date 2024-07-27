8d4b6e23b68340732b8cbce147d13efd22baca28
sanjeetgit1 <sksanjeet616@gmail.com>
2024-07-27 18:54:40 +0530

    >

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 // import path from "path";
 export const app = express();
-app.use(cors({origin:"https://backendcode-2.onrender.com/",credentials:true}))
-
+app.use(cors({origin:"http://localhost:3000/",credentials:true}))
+            
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cookieParser());
